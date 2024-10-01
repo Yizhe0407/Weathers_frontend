ba8bbd8a43c27b0d7dfa6b38ada33293c48f6df1
yizhe <liaoyizhe75@gmail.com>
2024-10-01 14:28:27 +0800

    Refactor user selection in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={inter.className} style={{ userSelect: 'none' }}>
           <Navbar />
           {children}
         </body>
